084e2d10f4834da59512cb332242e3d472bfd2f0
spadino <spada.andrea@gmail.com>
2018-08-18 17:45:03 -0300

    add a relative plugin

---
+++ UCR.Plugins/Remapper/AxisToRelativeAxis.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using HidWizards.UCR.Core.Attributes;
+using HidWizards.UCR.Core.Models;
+using HidWizards.UCR.Core.Models.Binding;
+using HidWizards.UCR.Core.Utilities;
+
+namespace HidWizards.UCR.Plugins.Remapper
+{
+    [Plugin("Axis to Relative axis")]
+    [PluginInput(DeviceBindingCategory.Range, "Axis")]
+    [PluginOutput(DeviceBindingCategory.Range, "Axis")]
+    public class AxisToRelativeAxis : Plugin
+    {
+        [PluginGui("Invert", ColumnOrder = 0)]
+        public bool Invert { get; set; }
+
+        [PluginGui("Linear", ColumnOrder = 3)]
+        public bool Linear { get; set; }
+
+        [PluginGui("Dead zone", ColumnOrder = 1)]
+        public int DeadZone { get; set; }
+
+        [PluginGui("Sensitivity", ColumnOrder = 2)]
+        public int Sensitivity { get; set; }
+
+        [PluginGui("Relative", ColumnOrder = 3)]
+        public bool Relative { get; set; }
+
+        public AxisToRelativeAxis()
+        {
+            DeadZone = 0;
+            Sensitivity = 100;
+            Relative = true;
+        }
+
+        public override void Update(params long[] values)
+        {
+            var value = values[0];
+            if (Invert) value *= -1;
+            if (DeadZone != 0) value = Functions.ApplyRangeDeadZone(value, DeadZone);
+            if (Sensitivity != 100) value = Functions.ApplyRangeSensitivity(value, Sensitivity, Linear);
+            if (Relative) value = Functions.ApplyRelativeIncrement(value);
+            value = Math.Min(Math.Max(value, Constants.AxisMinValue), Constants.AxisMaxValue);
+            WriteOutput(0, value);
+        }
+    }
+}
