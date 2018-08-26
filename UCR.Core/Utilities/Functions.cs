870d957f141959350783c7d4e6175b313ac27f99
spadino <spada.andrea@gmail.com>
2018-08-26 13:24:45 -0300

    Included an exponentail curve functions - used in AxisToDelta for better control near the center.

--- UCR.Core/Utilities/Functions.cs
+++ UCR.Core/Utilities/Functions.cs
@@ -18,15 +18,41 @@ namespace HidWizards.UCR.Core.Utilities
             if (linear) return (long)(value * sensitivityPercent);
 
             var sens = sensitivityPercent / 100d;
-            double AxisRange = 1d * (Constants.AxisMaxValue - Constants.AxisMinValue);
-            // Map value to -1 .. 1
-            double val11 = (((value - Constants.AxisMinValue) / AxisRange) * 2) - 1;
-            // calculate (Sensitivity * Value) + ( (1-Sensitivity) * Value^3 )
-            double valout = (sens * val11) + ((1 - sens) * Math.Pow( val11, 3 ));
-            // Map value back to AxisRange
-            value = (long) Math.Round( ((valout + 1) / 2d) * AxisRange + (1d * Constants.AxisMinValue) );
+            const double AxisRange = 1d * (Constants.AxisMaxValue - Constants.AxisMinValue);
 
-            return value;
+            // Map value to -1 .. 1
+            var valfx = (((value - Constants.AxisMinValue) / AxisRange) * 2) - 1;
+
+            // calculate (Sensitivity * Value) + ( (1-Sensitivity) * Value^3 )
+            var valout = (sens * valfx) + ((1 - sens) * Math.Pow(valfx, 3));
+
+            // Map value back to AxisRange
+            return (long)Math.Round(((valout + 1) / 2d) * AxisRange + (1d * Constants.AxisMinValue));
+        }
+
+        public static long ApplyCurvedResponse(long value, double curve)
+        {
+            var curveSens = curve;
+            long output = 0;
+
+            // Normalize the output so it always fit in the axis range
+            var curveRatio = (Math.Pow(Constants.AxisMaxValue, curveSens)) / Constants.AxisMaxValue;
+
+            // Apply an exponential curve
+            if (value > 0)
+            {
+                output = ((long)((Math.Pow(value, curveSens)) / curveRatio));
+            }
+            // for negative value, we need to convert them in positive, then in negative again after the
+            // exponential calculation
+            else if (value < 0)
+            {
+                value = value * -1;
+                value = ((long)((Math.Pow(value, curveSens)) / curveRatio));
+                output = value * -1;
+            }
+
+            return output;
         }
 
         public static long ApplyRelativeIncrement(long last, long prev, int sensitivity)
@@ -39,14 +65,7 @@ namespace HidWizards.UCR.Core.Utilities
         public static long HalfAxisToFullRange(long axis, bool positiveRange, bool invert)
         {
             long value;
-            if (positiveRange)
-            {
-                value = axis > 0L ? axis : 0L;
-            }
-            else
-            {
-                value = axis < 0 ? axis * -1 : 0L;
-            }
+            value = positiveRange ? axis > 0L ? axis : 0L : axis < 0 ? axis * -1 : 0L;
 
             value = Constants.AxisMinValue + value * 2;
 
--- UCR.Plugins/Remapper/AxisToDelta.cs
+++ UCR.Plugins/Remapper/AxisToDelta.cs
@@ -6,16 +6,13 @@ using HidWizards.UCR.Core.Utilities;
 
 namespace HidWizards.UCR.Plugins.Remapper
 {
-    [Plugin("Axis to axis (delta)")]
+    [Plugin("Axis to axis (Delta)")]
     [PluginInput(DeviceBindingCategory.Range, "Axis")]
     [PluginOutput(DeviceBindingCategory.Range, "Axis")]
     public class AxisToDelta : Plugin
     {
         [PluginGui("Invert", ColumnOrder = 0)]
         public bool Invert { get; set; }
-
-        [PluginGui("Linear", ColumnOrder = 3)]
-        public bool Linear { get; set; }
 
         [PluginGui("Dead zone", ColumnOrder = 1)]
         public int DeadZone { get; set; }
@@ -26,6 +23,9 @@ namespace HidWizards.UCR.Plugins.Remapper
         [PluginGui("Multiplier", ColumnOrder = 3)]
         public int Multiplier { get; set; }
 
+        [PluginGui("Curve", ColumnOrder = 4)]
+        public double Curve { get; set; }
+
         private long AxisRest;
 
         public AxisToDelta()
@@ -33,6 +33,7 @@ namespace HidWizards.UCR.Plugins.Remapper
             DeadZone = 0;
             Sensitivity = 100;
             Multiplier = 20;
+            Curve = 1.2;
             AxisRest = 0;
         }
 
@@ -41,12 +42,23 @@ namespace HidWizards.UCR.Plugins.Remapper
             var value = values[0];
             var delta = value - AxisRest;
 
-            if (Invert) delta *= -1;
-            if (DeadZone != 0) delta = Functions.ApplyRangeDeadZone(delta, DeadZone);
-            if (Sensitivity != 100) delta = Functions.ApplyRangeSensitivity(delta, Sensitivity, Linear);
-
+            // Alter the response
             delta = delta * Multiplier;
 
+            // Invert
+            if (Invert) delta *= -1;
+
+            // Apply a deadzone
+            if (DeadZone != 0) delta = Functions.ApplyRangeDeadZone(delta, DeadZone);
+
+            // Apply a linear sensitivity to the input
+            // (only "linear response" has some use dealing with deltas...)
+            if (Sensitivity != 100) delta = Functions.ApplyRangeSensitivity(delta, Sensitivity, true);
+
+            // Apply a curved response to the output
+            if (Curve != 0) delta = Functions.ApplyCurvedResponse(delta, Curve);
+
+            // Kepp output in range
             delta = Math.Min(Math.Max(delta, Constants.AxisMinValue), Constants.AxisMaxValue);
             WriteOutput(0, delta);
 
