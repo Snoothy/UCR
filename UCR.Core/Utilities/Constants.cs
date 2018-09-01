baa16c8d3a2162ccd3846a48312dc73ffed42dfb
evilC <evilc@evilc.com>
2018-09-01 21:48:34 +0100

    Format summary

--- UCR.Core/Utilities/Constants.cs
+++ UCR.Core/Utilities/Constants.cs
@@ -4,10 +4,10 @@
     {
         public const int MaxDevices = 16;
         /// <summary>
-        /// When doing calculations using the size of the range from the mid-point to the extreme...
-        /// ...use <see cref="AxisMaxAbsValue"/> NOT <see cref="AxisMaxValue"/>.
-        /// Signed numbers are larger on the negative side, so perform calculations using the size of the negative scale...
-        /// ...and then clamp the value 
+        /// <para>When doing calculations using the size of the range from the mid-point to the extreme,
+        /// use <see cref="AxisMaxAbsValue"/> NOT <see cref="AxisMaxValue"/></para>
+        /// <para>Signed numbers are larger on the negative side, so perform calculations using the size of the negative scale,
+        /// and then clamp the value</para>
         /// </summary>
         public const int AxisMaxAbsValue = short.MinValue * -1; // For short, this should be 32768
         public const int AxisMaxValue = short.MaxValue; // ToDo: This will be 32767, meaning that negative scaling does not go to -32768. Fix.
