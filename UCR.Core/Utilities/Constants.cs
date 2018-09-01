8e198dc35af54eb5bfa9022f677317899c089509
evilC <evilc@evilc.com>
2018-09-01 20:30:13 +0100

    ToDo

--- UCR.Core/Utilities/Constants.cs
+++ UCR.Core/Utilities/Constants.cs
@@ -3,7 +3,7 @@
     public static class Constants
     {
         public const int MaxDevices = 16;
-        public const int AxisMaxValue = short.MaxValue;
+        public const int AxisMaxValue = short.MaxValue; // ToDo: This will be 32767, meaning that negative scaling does not go to -32768. Fix.
         public const int AxisMinValue = short.MinValue;
     }
 }
