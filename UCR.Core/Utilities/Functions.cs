5b144b4608b59438d21bc30ed81c85923115a0d0
evilC <evilc@evilc.com>
2018-09-01 19:29:27 +0100

    Copy in changes from develop

--- UCR.Core/Utilities/Functions.cs
+++ UCR.Core/Utilities/Functions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace HidWizards.UCR.Core.Utilities
 {
@@ -22,18 +23,11 @@ namespace HidWizards.UCR.Core.Utilities
             // Map value to -1 .. 1
             double val11 = (((value - Constants.AxisMinValue) / AxisRange) * 2) - 1;
             // calculate (Sensitivity * Value) + ( (1-Sensitivity) * Value^3 )
-            double valout = (sens * val11) + ((1 - sens) * Math.Pow( val11, 3 ));
+            double valout = (sens * val11) + ((1 - sens) * Math.Pow(val11, 3));
             // Map value back to AxisRange
-            value = (long) Math.Round( ((valout + 1) / 2d) * AxisRange + (1d * Constants.AxisMinValue) );
+            value = (long)Math.Round(((valout + 1) / 2d) * AxisRange + (1d * Constants.AxisMinValue));
 
             return value;
-        }
-
-        public static long ApplyRelativeIncrement(long last, long prev, int sensitivity)
-        {
-            var sensitivityPercent = (sensitivity / 100.0);
-            last = (long)(last * sensitivityPercent);
-            return last + prev;
         }
 
         public static long HalfAxisToFullRange(long axis, bool positiveRange, bool invert)
@@ -49,8 +43,9 @@ namespace HidWizards.UCR.Core.Utilities
             }
 
             value = Constants.AxisMinValue + value * 2;
-
-            return invert ? value * -1 : value;
+            value = invert ? value * -1 : value;
+            value = Math.Min(Math.Max(value, Constants.AxisMinValue), Constants.AxisMaxValue);
+            return value;
         }
     }
 }
