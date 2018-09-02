1dd7d5bf61e3e63c107e27a05f75452b22c2c776
evilC <evilc@evilc.com>
2018-09-02 12:14:06 +0100

    Add Unit Test for Invert

---
+++ UCR.Tests/HelperTests/FunctionTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HidWizards.UCR.Core.Utilities;
+using NUnit.Framework;
+
+namespace HidWizards.UCR.Tests.HelperTests
+{
+    [TestFixture]
+    public class FunctionTests
+    {
+        [Test]
+        public void InvertTest()
+        {
+            Assert.AreEqual(Functions.Invert(0), 0);
+            Assert.AreEqual(Functions.Invert(32767), -32768);
+            Assert.AreEqual(Functions.Invert(-32768), 32767);
+            Assert.AreEqual(Functions.Invert(1), -1);
+            Assert.AreEqual(Functions.Invert(-1), 1);
+        }
+    }
+}
