c7a1e2e500022d4e39059b5a74dd1cbc3e48d414
evilC <evilc@evilc.com>
2018-09-02 12:22:27 +0100

    Add Clamp Test, tidy Invert

--- UCR.Tests/HelperTests/FunctionTests.cs
+++ UCR.Tests/HelperTests/FunctionTests.cs
@@ -15,10 +15,22 @@ namespace HidWizards.UCR.Tests.HelperTests
         public void InvertTest()
         {
             Assert.AreEqual(Functions.Invert(0), 0);
-            Assert.AreEqual(Functions.Invert(32767), -32768);
-            Assert.AreEqual(Functions.Invert(-32768), 32767);
+            Assert.AreEqual(Functions.Invert(Constants.AxisMaxValue), Constants.AxisMinValue);
+            Assert.AreEqual(Functions.Invert(Constants.AxisMinValue), Constants.AxisMaxValue);
             Assert.AreEqual(Functions.Invert(1), -1);
             Assert.AreEqual(Functions.Invert(-1), 1);
+        }
+
+        [Test]
+        public void ClampTest()
+        {
+            Assert.AreEqual(Functions.ClampAxisRange(Constants.AxisMinValue - 1), Constants.AxisMinValue);
+            Assert.AreEqual(Functions.ClampAxisRange(Constants.AxisMaxValue + 1), Constants.AxisMaxValue);
+            Assert.AreEqual(Functions.ClampAxisRange(Constants.AxisMaxValue), Constants.AxisMaxValue);
+            Assert.AreEqual(Functions.ClampAxisRange(Constants.AxisMinValue), Constants.AxisMinValue);
+            Assert.AreEqual(Functions.ClampAxisRange(0), 0);
+            Assert.AreEqual(Functions.ClampAxisRange(1), 1);
+            Assert.AreEqual(Functions.ClampAxisRange(-1), -1);
         }
     }
 }
