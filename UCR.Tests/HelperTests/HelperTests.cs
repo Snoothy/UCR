1c914534239c08f26c2096c1e6112eb7c7f562a6
evilC <evilc@evilc.com>
2018-09-02 12:30:49 +0100

    Add DZ Helper Test stub

---
+++ UCR.Tests/HelperTests/HelperTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HidWizards.UCR.Core.Utilities;
+using HidWizards.UCR.Core.Utilities.AxisHelpers;
+using NUnit.Framework;
+
+namespace HidWizards.UCR.Tests.HelperTests
+{
+    [TestFixture]
+    public class HelperTests
+    {
+        [Test]
+        public void DeadZoneHelperTest()
+        {
+            var helper = new DeadZoneHelper();
+            // On initialize, the helper should work for DZ 0 without setting Percent
+            Assert.AreEqual(helper.ApplyRangeDeadZone(Constants.AxisMaxValue), Constants.AxisMaxValue);
+        }
+    }
+}
