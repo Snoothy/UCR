5b0b3d8020f0613f9cdba3ba428176d924ae9e12
evilC <evilc@evilc.com>
2018-09-03 17:23:16 +0100

    Fix naming

--- UCR.Tests/UtilityTests/HelperTests/CircularDeadzoneHelperTests.cs
+++ UCR.Tests/UtilityTests/HelperTests/CircularDeadzoneHelperTests.cs
@@ -10,7 +10,7 @@ using NUnit.Framework;
 namespace HidWizards.UCR.Tests.UtilityTests.HelperTests
 {
     [TestFixture]
-    public class CircularDeadzoneHelperTests
+    public class CircularDeadZoneHelperTests
     {
         [TestCase(Constants.AxisMaxValue, 0, ExpectedResult = new long[] { Constants.AxisMaxValue, 0 }, TestName = "CircularDeadZoneHelper (Init): Max returns Max")]
         [TestCase(Constants.AxisMinValue, 0, ExpectedResult = new long[] { Constants.AxisMinValue, 0 }, TestName = "CircularDeadZoneHelper (Init): Min returns Min")]
