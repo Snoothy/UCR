a7aee848be486cb46a804db0a3ba8d41c3ae9974
evilC <evilc@evilc.com>
2018-09-03 15:52:10 +0100

    Parameterize tests

--- UCR.Tests/UtilityTests/HelperTests.cs
+++ UCR.Tests/UtilityTests/HelperTests.cs
@@ -7,58 +7,64 @@ namespace HidWizards.UCR.Tests.UtilityTests
     [TestFixture]
     public class HelperTests
     {
-        [Test]
-        public void DeadZoneHelperInitTest()
+        [TestCase(Constants.AxisMaxValue, ExpectedResult = Constants.AxisMaxValue, TestName = "SensitivityHelper (Init): Max returns Max")]
+        [TestCase(Constants.AxisMinValue, ExpectedResult = Constants.AxisMinValue, TestName = "SensitivityHelper (Init): Min returns Min")]
+        [TestCase(0, ExpectedResult = 0, TestName = "SensitivityHelper (Init): 0 returns 0")]
+        [TestCase(1, ExpectedResult = 1, TestName = "SensitivityHelper (Init): 1 returns 1")]
+        [TestCase(-1, ExpectedResult = -1, TestName = "SensitivityHelper (Init): -1 returns -1")]
+        public long DeadZoneHelperInitTests(long inputValue)
         {
-            var helper = new DeadZoneHelper {Percentage = 20};
-            // On initialize, the helper should work for DZ 0 without setting Percent
-            Assert.AreEqual(Constants.AxisMaxValue, helper.ApplyRangeDeadZone(Constants.AxisMaxValue));
-            Assert.AreEqual(Constants.AxisMinValue, helper.ApplyRangeDeadZone(Constants.AxisMinValue));
-            Assert.AreEqual(0, helper.ApplyRangeDeadZone(0));
-            Assert.AreEqual(1, helper.ApplyRangeDeadZone(1));
-            Assert.AreEqual(-1, helper.ApplyRangeDeadZone(-1));
+            var helper = new DeadZoneHelper();
+            return helper.ApplyRangeDeadZone(inputValue);
         }
 
-        [Test]
-        public void DeadZoneHelperValuesTest()
+        [TestCase(Constants.AxisMaxValue, 50, ExpectedResult = Constants.AxisMaxValue, TestName = "SensitivityHelper (50): Max returns Max")]
+        [TestCase(Constants.AxisMinValue, 50, ExpectedResult = Constants.AxisMinValue, TestName = "SensitivityHelper (50): Min returns Min")]
+        [TestCase(0, 50, ExpectedResult = 0, TestName = "SensitivityHelper (50): 0 returns 0")]
+        public long DeadZoneHelperValueTests(long inputValue, int percentage)
         {
-            var helper = new DeadZoneHelper {Percentage = 50};
-            // At 50% DZ, we should still be able to reach extremes
-            var max = helper.ApplyRangeDeadZone(Constants.AxisMaxValue);
-            //Assert.AreEqual(Constants.AxisMaxValue, max);
-            var min = helper.ApplyRangeDeadZone(Constants.AxisMinValue);
-            Assert.AreEqual(Constants.AxisMinValue, min);
-            Assert.AreEqual(0, helper.ApplyRangeDeadZone(0));
+            var helper = new DeadZoneHelper {Percentage = percentage};
+            return helper.ApplyRangeDeadZone(inputValue);
         }
 
-        [Test]
-        public void SensitivityTest()
+        [TestCase(Constants.AxisMaxValue, ExpectedResult = Constants.AxisMaxValue, TestName = "SensitivityHelper (Init): Max returns Max")]
+        [TestCase(Constants.AxisMinValue, ExpectedResult = Constants.AxisMinValue, TestName = "SensitivityHelper (Init): Min returns Min")]
+        [TestCase(0, ExpectedResult = 0, TestName = "SensitivityHelper (Init): 0 returns 0")]
+        public long SensitivityHelperInitTests(long inputValue)
         {
             var helper = new SensitivityHelper();
-            Assert.AreEqual(Constants.AxisMinValue, helper.ApplyRangeSensitivity(Constants.AxisMinValue));
-            Assert.AreEqual(Constants.AxisMaxValue, helper.ApplyRangeSensitivity(Constants.AxisMaxValue));
-            Assert.AreEqual(0, helper.ApplyRangeSensitivity(0));
-            helper.Percentage = 50;
-            Assert.AreEqual(Constants.AxisMinValue, helper.ApplyRangeSensitivity(Constants.AxisMinValue));
-            Assert.AreEqual(Constants.AxisMaxValue, helper.ApplyRangeSensitivity(Constants.AxisMaxValue));
-            helper.Percentage = 20;
-            Assert.AreEqual(Constants.AxisMinValue, helper.ApplyRangeSensitivity(Constants.AxisMinValue));
-            Assert.AreEqual(Constants.AxisMaxValue, helper.ApplyRangeSensitivity(Constants.AxisMaxValue));
-            Assert.AreEqual(0, helper.ApplyRangeSensitivity(0));
+            return helper.ApplyRangeSensitivity(inputValue);
         }
 
-        [Test]
-        public void CircularDeadZoneTest()
+        [TestCase(Constants.AxisMaxValue, 50, ExpectedResult = Constants.AxisMaxValue, TestName = "SensitivityHelper (50): Max returns Max")]
+        [TestCase(Constants.AxisMinValue, 50, ExpectedResult = Constants.AxisMinValue, TestName = "SensitivityHelper (50): Min returns Min")]
+        [TestCase(Constants.AxisMaxValue, 20, ExpectedResult = Constants.AxisMaxValue, TestName = "SensitivityHelper (20): Max returns Max")]
+        [TestCase(Constants.AxisMinValue, 20, ExpectedResult = Constants.AxisMinValue, TestName = "SensitivityHelper (20): Min returns Min")]
+        public long SensitivityHelperValueTests(long inputValue, int percentage)
+        {
+            var helper = new SensitivityHelper {Percentage = percentage};
+            return helper.ApplyRangeSensitivity(inputValue);
+        }
+
+        [TestCase(Constants.AxisMaxValue, 0, ExpectedResult = new long[]{Constants.AxisMaxValue, 0}, TestName = "CircularDeadZoneHelper (Init): Max returns Max")]
+        [TestCase(Constants.AxisMinValue, 0, ExpectedResult = new long[]{Constants.AxisMinValue, 0}, TestName = "CircularDeadZoneHelper (Init): Min returns Min")]
+        [TestCase(0, 0, ExpectedResult = new long[]{0, 0}, TestName = "CircularDeadZoneHelper (Init): 0 returns 0")]
+        public long[] CircularDeadZoneInitTests(long x, long y)
         {
             var helper = new CircularDeadZoneHelper();
-            Assert.AreEqual(new long[] { Constants.AxisMaxValue, 0 }, helper.ApplyRangeDeadZone(new long[] { Constants.AxisMaxValue, 0 }));
-            Assert.AreEqual(new long[] { Constants.AxisMinValue, 0 }, helper.ApplyRangeDeadZone(new long[] { Constants.AxisMinValue, 0 }));
-            helper.Percentage = 50;
-            Assert.AreEqual(new long[] {Constants.AxisMaxValue, 0}, helper.ApplyRangeDeadZone(new long[] { Constants.AxisMaxValue, 0 }));
-            Assert.AreEqual(new long[] {Constants.AxisMinValue, 0}, helper.ApplyRangeDeadZone(new long[] { Constants.AxisMinValue, 0 }));
-            helper.Percentage = 20;
-            Assert.AreEqual(new long[] {Constants.AxisMaxValue, 0}, helper.ApplyRangeDeadZone(new long[] { Constants.AxisMaxValue, 0 }));
-            Assert.AreEqual(new long[] {Constants.AxisMinValue, 0}, helper.ApplyRangeDeadZone(new long[] { Constants.AxisMinValue, 0 }));
+            return helper.ApplyRangeDeadZone(new[] {x, y});
+        }
+
+        [TestCase(Constants.AxisMaxValue, 0, 50, ExpectedResult = new long[] { Constants.AxisMaxValue, 0 }, TestName = "CircularDeadZoneHelper (50): Max returns Max")]
+        [TestCase(Constants.AxisMinValue, 0, 50, ExpectedResult = new long[] { Constants.AxisMinValue, 0 }, TestName = "CircularDeadZoneHelper (50): Min returns Min")]
+        [TestCase(0, 0, 50, ExpectedResult = new long[] { 0, 0 }, TestName = "CircularDeadZoneHelper (50): 0 returns 0")]
+        [TestCase(Constants.AxisMaxValue, 0, 20, ExpectedResult = new long[] { Constants.AxisMaxValue, 0 }, TestName = "CircularDeadZoneHelper (20): Max returns Max")]
+        [TestCase(Constants.AxisMinValue, 0, 20, ExpectedResult = new long[] { Constants.AxisMinValue, 0 }, TestName = "CircularDeadZoneHelper (20): Min returns Min")]
+        [TestCase(0, 0, 20, ExpectedResult = new long[] { 0, 0 }, TestName = "CircularDeadZoneHelper (20): 0 returns 0")]
+        public long[] CircularDeadZoneValueTests(long x, long y, int percentage)
+        {
+            var helper = new CircularDeadZoneHelper {Percentage = percentage};
+            return helper.ApplyRangeDeadZone(new[] {x, y});
         }
     }
 }
