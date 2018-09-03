10426dfc348133ee10dfe2431c5c88e7cc53ba38
evilC <evilc@evilc.com>
2018-09-03 18:54:23 +0100

    Remove deprecated functions

--- UCR.Core/Utilities/Functions.cs
+++ UCR.Core/Utilities/Functions.cs
@@ -36,13 +36,11 @@ namespace HidWizards.UCR.Core.Utilities
                 if (axis < 0) return 0;
                 value = axis;
                 if (value == Constants.AxisMaxValue) value++;
-                //value = axis > 0L ? axis : 0L;
             }
             else
             {
                 if (axis > 0) return 0;
                 value = axis * -1;
-                //value = axis < 0 ? axis * -1 : 0L;
             }
 
             value *= 2;
@@ -53,51 +51,5 @@ namespace HidWizards.UCR.Core.Utilities
             return value;
         }
 
-        [Obsolete("Deprecated, use DeadZoneHelper instead.")]
-        public static long ApplyRangeDeadZone(long value, int deadZonePercentage)
-        {
-            var gap = (deadZonePercentage / 100.0) * Constants.AxisMaxValue;
-            var remainder = Constants.AxisMaxValue - gap;
-            var gapPercent = Math.Max(0, Math.Abs(value) - gap) / remainder;
-            return (long)(gapPercent * Constants.AxisMaxValue * Math.Sign(value));
-        }
-
-        [Obsolete("Deprecated, use SensitivityHelper instead.")]
-        public static long ApplyRangeSensitivity(long value, int sensitivity, bool linear)
-        {
-            var sensitivityPercent = (sensitivity / 100.0);
-            if (linear) return (long)(value * sensitivityPercent);
-
-            var sens = sensitivityPercent / 100d;
-            double AxisRange = 1d * (Constants.AxisMaxValue - Constants.AxisMinValue);
-            // Map value to -1 .. 1
-            double val11 = (((value - Constants.AxisMinValue) / AxisRange) * 2) - 1;
-            // calculate (Sensitivity * Value) + ( (1-Sensitivity) * Value^3 )
-            double valout = (sens * val11) + ((1 - sens) * Math.Pow( val11, 3 ));
-            // Map value back to AxisRange
-            value = (long) Math.Round( ((valout + 1) / 2d) * AxisRange + (1d * Constants.AxisMinValue) );
-
-            return value;
-        }
-
-        //ToDo: Does not properly invert, may fail with -32768
-        [Obsolete("Deprecated, use SplitAxis instead.")]
-        public static long HalfAxisToFullRange(long axis, bool positiveRange, bool invert)
-        {
-            long value;
-            if (positiveRange)
-            {
-                value = axis > 0L ? axis : 0L;
-            }
-            else
-            {
-                value = axis < 0 ? axis * -1 : 0L;
-            }
-
-            value = Constants.AxisMinValue + value * 2;
-            value = invert ? value * -1 : value;
-            value = Math.Min(Math.Max(value, Constants.AxisMinValue), Constants.AxisMaxValue);
-            return value; 
-        }
     }
 }
