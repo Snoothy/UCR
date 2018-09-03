6f82232f2acec8ce37a39c725e4bb2b986b4b41d
evilC <evilc@evilc.com>
2018-09-03 15:13:49 +0100

    FunctionTests -> UtilityTests

--- UCR.Tests/UtilityTests/FunctionTests.cs
+++ UCR.Tests/UtilityTests/FunctionTests.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using HidWizards.UCR.Core.Utilities;
+﻿using HidWizards.UCR.Core.Utilities;
 using NUnit.Framework;
 
-namespace HidWizards.UCR.Tests.HelperTests
+namespace HidWizards.UCR.Tests.UtilityTests
 {
     [TestFixture]
     public class FunctionTests
