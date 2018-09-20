01b15bdda52bf679d6106227ee7e1867e12bee20
Kai Ejler Rasmussen <kai.ejler.rasmussen@gmail.com>
2018-09-21 00:09:26 +0200

    Updated Nuke to properly run tests and build artifacts

--- build/Build.cs
+++ build/Build.cs
@@ -1,31 +1,54 @@
-using System;
-using System.Linq;
+using System.IO;
 using Nuke.Common;
+using Nuke.Common.BuildServers;
+using Nuke.Common.Git;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tools.MSBuild;
 using Nuke.Common.Tools.NuGet;
+using Nuke.Common.Tools.Nunit;
+using Nuke.Common.Tools.GitVersion;
 using Nuke.Core.Tooling;
+using ToolSettingsExtensions = Nuke.Common.Tooling.ToolSettingsExtensions;
+
 using static Nuke.Common.Tools.Git.GitTasks;
 using static Nuke.Common.Tools.MSBuild.MSBuildTasks;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
-using ToolSettingsExtensions = Nuke.Common.Tooling.ToolSettingsExtensions;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using static Nuke.Common.Tools.GitVersion.GitVersionTasks;
+using static Nuke.Common.Tools.Nunit.NunitTasks;
+using static Nuke.Compression.CompressionTasks;
+
 
 class Build : NukeBuild
 {
     public static int Main () => Execute<Build>(x => x.Test);
 
+    [Parameter] string CodeAnalysis;
+
     [Solution] readonly Solution Solution;
+    [GitRepository] readonly GitRepository GitRepository;
 
     string IoWrapper => "IOWrapper";
     AbsolutePath IoWrapperDirectory => RootDirectory / "submodules" / IoWrapper;
     AbsolutePath IoWrapperSolution => IoWrapperDirectory / (IoWrapper + ".sln");
+    AbsolutePath UcrOutputDirectory => RootDirectory / "UCR" / "bin" / Configuration;
+    AbsolutePath TestDirectory => RootDirectory / "UCR.Tests";
 
-    MSBuildSettings DefaultIoWrapperMSBuild => ToolSettingsExtensions.SetWorkingDirectory(DefaultMSBuildCompile, IoWrapperDirectory).SetSolutionFile(IoWrapperSolution);
+    MSBuildSettings DefaultIoWrapperMSBuild => ToolSettingsExtensions
+        .SetWorkingDirectory(DefaultMSBuildCompile, IoWrapperDirectory)
+        .SetSolutionFile(IoWrapperSolution)
+        .SetVerbosity(MSBuildVerbosity.Quiet);
 
+    Target CleanArtifacts => _ => _
+        .Executes(() =>
+        {
+            EnsureCleanDirectory(ArtifactsDirectory);
+        });
 
     Target Clean => _ => _
+        .DependsOn(CleanArtifacts)
         .Executes(() =>
         {
             DeleteDirectories(GlobDirectories(RootDirectory, "**/bin", "**/obj"));
@@ -37,9 +60,6 @@ class Build : NukeBuild
         {
             Git("submodule init");
             Git("submodule update");
-            MSBuild(s => DefaultIoWrapperMSBuild
-                .SetTargets("Restore")
-            );
             NuGetTasks.NuGetRestore(s => s.SetTargetPath(IoWrapperSolution));
         });
 
@@ -48,7 +68,8 @@ class Build : NukeBuild
         .Executes(() =>
         {
             MSBuild(s => DefaultIoWrapperMSBuild
-                .SetTargets("Rebuild")
+                .SetTargets("Restore","Rebuild")
+                .SetConfiguration(Configuration)
             );
         });
 
@@ -61,7 +82,6 @@ class Build : NukeBuild
         });
 
     Target Restore => _ => _
-        .DependsOn(Clean)
         .Executes(() =>
         {
             NuGetTasks.NuGetRestore(s => s.SetTargetPath(SolutionFile));
@@ -71,17 +91,101 @@ class Build : NukeBuild
             );
         });
 
-    Target Compile => _ => _
-        .DependsOn(Restore)
+    Target StartCodeAnalysis => _ => _
+        .OnlyWhen(() => !string.IsNullOrEmpty(CodeAnalysis))
         .Executes(() =>
         {
-            MSBuild(s => DefaultMSBuildCompile);
+            DotNet($"sonarscanner begin /k:\"Snoothy_UCR\" /d:sonar.organization=\"snoothy-github\" /d:sonar.host.url=\"https://sonarcloud.io\" /d:sonar.login=\"cad188647aee521b62439577ebe235d6a61e750c\" /v:sonar.projectVersion=\"{GetFullSemanticVersion()}\"");
         });
+
+    Target Versioning => _ => _
+        .Executes(() =>
+        {
+            // TODO Update readme version
+
+            // TODO Update appveyors version
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .DependsOn(StartCodeAnalysis)
+        .Executes(() =>
+        {
+            MSBuild(s => s
+                .SetTargetPath(SolutionFile)
+                .SetConfiguration(Configuration)
+                .SetTargets("Rebuild")
+                .SetVerbosity(MSBuildVerbosity.Minimal)
+                // TODO This doesn't set all assembly versions
+                .SetAssemblyVersion(GetCurrentVersion())
+                .SetFileVersion(GetCurrentVersion())
+                .SetInformationalVersion(GetFullSemanticVersion())
+            );
+        });
+
+    Target EndCodeAnalysis => _ => _
+    .OnlyWhen(() => !string.IsNullOrEmpty(CodeAnalysis))
+    .Executes(() =>
+    {
+        DotNet($"sonarscanner end /d:sonar.login=\"cad188647aee521b62439577ebe235d6a61e750c\"");
+    });
 
     Target Test => _ => _
         .DependsOn(Compile)
         .Executes(() =>
         {
-            MSBuild(s => DefaultMSBuildCompile);
+            EnsureExistingDirectory("Providers");
+            EnsureExistingDirectory("Plugins");
+            Nunit3(s => s
+                .AddInputFiles(GlobFiles(TestDirectory, $"**/bin/{Configuration}/UCR.Tests.dll").NotEmpty())
+                .EnableNoResults()
+            );
         });
+    
+    Target Artifacts => _ => _
+        .DependsOn(CleanArtifacts)
+        .DependsOn(EndCodeAnalysis)
+        .DependsOn(Test)
+        .Executes(() =>
+        {
+            if (!Directory.Exists(ArtifactsDirectory)) Directory.CreateDirectory(ArtifactsDirectory);
+            CopyDirectoryRecursively(UcrOutputDirectory, ArtifactsDirectory, FileExistsPolicy.Overwrite);
+            
+            CompressZip(ArtifactsDirectory, $"artifacts/UCR_{GetSemanticVersion()}.zip");
+        });
+
+    Target Changelog => _ => _
+        .DependsOn(Versioning)
+        .Executes(() =>
+        {
+            // TODO keep a change log update
+        });
+
+    Target Publish => _ => _
+        .DependsOn(Versioning)
+        .DependsOn(Artifacts)
+        .DependsOn(Changelog)
+        .Executes(() =>
+        {
+            // TODO
+        });
+
+    private string GetCurrentVersion()
+    {
+        // TODO GitVersion
+        return "0.4.0";
+    }
+
+    private string GetSemanticVersion()
+    {
+        return $"v{GetCurrentVersion()}";
+    }
+
+    private string GetFullSemanticVersion()
+    {
+        string additionalVersion = "";
+        if (AppVeyor.Instance != null) additionalVersion = $"+{AppVeyor.Instance.BuildNumber}";
+        return $"v{GetCurrentVersion()}-{GitRepository.Branch}{additionalVersion}";
+    }
+
 }
