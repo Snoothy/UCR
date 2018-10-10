18781d7b6bda79108ee29927333bc8d3de4454d4
spadino <spada.andrea@gmail.com>
2018-10-10 17:23:50 -0300

    Add a 3axes plugin. Rename AxesToAxes.

--- UCR.Plugins/Remapper/AxesToAxes_3.cs
+++ UCR.Plugins/Remapper/AxesToAxes_3.cs
@@ -7,28 +7,33 @@ using HidWizards.UCR.Core.Utilities.AxisHelpers;
 
 namespace HidWizards.UCR.Plugins.Remapper
 {
-    [Plugin("Axes to Axes")]
+    [Plugin("Axes to Axes (3)")]
     [PluginInput(DeviceBindingCategory.Range, "X Axis")]
     [PluginInput(DeviceBindingCategory.Range, "Y Axis")]
+	[PluginInput(DeviceBindingCategory.Range, "Z Axis")]
     [PluginOutput(DeviceBindingCategory.Range, "X Axis")]
     [PluginOutput(DeviceBindingCategory.Range, "Y Axis")]
-    public class AxesToAxes : Plugin
+	[PluginOutput(DeviceBindingCategory.Range, "Z Axis")]
+    public class AxesToAxes_3 : Plugin
     {
         private readonly CircularDeadZoneHelper _circularDeadZoneHelper = new CircularDeadZoneHelper();
         private readonly DeadZoneHelper _deadZoneHelper = new DeadZoneHelper();
         private readonly SensitivityHelper _sensitivityHelper = new SensitivityHelper();
-        private double _linearSenstitivityScaleFactor;
+        private double _linearSensitivityScaleFactor;
 
         [PluginGui("Invert X", ColumnOrder = 0)]
         public bool InvertX { get; set; }
 
         [PluginGui("Invert Y", ColumnOrder = 1)]
         public bool InvertY { get; set; }
+		
+		[PluginGui("Invert Z", ColumnOrder = 2)]
+		public bool InvertZ { get; set; }
 
-        [PluginGui("Sensitivity", ColumnOrder = 2)]
+        [PluginGui("Sensitivity", ColumnOrder = 3)]
         public int Sensitivity { get; set; }
 
-        [PluginGui("Linear", RowOrder = 0, ColumnOrder = 2)]
+        [PluginGui("Linear", RowOrder = 0, ColumnOrder = 4)]
         public bool Linear { get; set; }
 
         [PluginGui("Dead zone", RowOrder = 1, ColumnOrder = 0)]
@@ -38,7 +43,7 @@ namespace HidWizards.UCR.Plugins.Remapper
         public bool CircularDz { get; set; }
 
 
-        public AxesToAxes()
+        public AxesToAxes_3()
         {
             DeadZone = 0;
             Sensitivity = 100;
@@ -54,12 +59,12 @@ namespace HidWizards.UCR.Plugins.Remapper
             _deadZoneHelper.Percentage = DeadZone;
             _circularDeadZoneHelper.Percentage = DeadZone;
             _sensitivityHelper.Percentage = Sensitivity;
-            _linearSenstitivityScaleFactor = ((double)Sensitivity / 100);
+            _linearSensitivityScaleFactor = ((double)Sensitivity / 100);
         }
 
         public override void Update(params long[] values)
         {
-            var outputValues = new long[] {values[0], values[1]};
+            var outputValues = new long[] {values[0], values[1], values[2]};
             if (DeadZone != 0)
             {
                 if (CircularDz)
@@ -70,6 +75,7 @@ namespace HidWizards.UCR.Plugins.Remapper
                 {
                     outputValues[0] = _deadZoneHelper.ApplyRangeDeadZone(outputValues[0]);
                     outputValues[1] = _deadZoneHelper.ApplyRangeDeadZone(outputValues[1]);
+					outputValues[2] = _deadZoneHelper.ApplyRangeDeadZone(outputValues[2]);
                 }
                 
             }
@@ -77,24 +83,29 @@ namespace HidWizards.UCR.Plugins.Remapper
             {
                 if (Linear)
                 {
-                    outputValues[0] = (long)(outputValues[0] * _linearSenstitivityScaleFactor);
-                    outputValues[1] = (long)(outputValues[1] * _linearSenstitivityScaleFactor);
+                    outputValues[0] = (long)(outputValues[0] * _linearSensitivityScaleFactor);
+                    outputValues[1] = (long)(outputValues[1] * _linearSensitivityScaleFactor);
+					outputValues[2] = (long)(outputValues[2] * _linearSensitivityScaleFactor);
                 }
                 else
                 {
                     outputValues[0] = _sensitivityHelper.ApplyRangeSensitivity(outputValues[0]);
                     outputValues[1] = _sensitivityHelper.ApplyRangeSensitivity(outputValues[1]);
+					outputValues[2] = _sensitivityHelper.ApplyRangeSensitivity(outputValues[2]);
                 }
             }
 
             outputValues[0] = Functions.ClampAxisRange(outputValues[0]);
             outputValues[1] = Functions.ClampAxisRange(outputValues[1]);
+			outputValues[2] = Functions.ClampAxisRange(outputValues[2]);
 
             if (InvertX) outputValues[0] = Functions.Invert(outputValues[0]);
             if (InvertY) outputValues[1] = Functions.Invert(outputValues[1]);
+			if (InvertZ) outputValues[2] = Functions.Invert(outputValues[2]);
 
             WriteOutput(0, outputValues[0]);
             WriteOutput(1, outputValues[1]);
+			WriteOutput(2, outputValues[2]);
         }
     }
 }
