75f4023f1d966c6dbbbb1ad7311204396cde147f
Kai Ejler Rasmussen <kai.ejler.rasmussen@gmail.com>
2018-12-27 02:12:55 +0100

    Add device input and output lists for profile details on the dashboard

--- UCR/ViewModels/Dashboard/DashboardViewModel.cs
+++ UCR/ViewModels/Dashboard/DashboardViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
@@ -6,6 +7,7 @@ using System.Windows;
 using HidWizards.UCR.Core;
 using HidWizards.UCR.Core.Annotations;
 using HidWizards.UCR.Core.Models;
+using HidWizards.UCR.Core.Models.Binding;
 
 namespace HidWizards.UCR.ViewModels.Dashboard
 {
@@ -16,6 +18,8 @@ namespace HidWizards.UCR.ViewModels.Dashboard
         public Visibility ProfileDetailsActive => SelectedProfileItem != null ? Visibility.Visible : Visibility.Hidden;
         public bool CanActivateProfile => SelectedProfileItem != null;
         public bool CanDeactivateProfile => Context?.ActiveProfile != null;
+        public ObservableCollection<DeviceItem> InputDevices { get; set; }
+        public ObservableCollection<DeviceItem> OutputDevices { get; set; }
 
         private ProfileItem _selectedProfileItem = null;
         public ProfileItem SelectedProfileItem
@@ -39,8 +43,40 @@ namespace HidWizards.UCR.ViewModels.Dashboard
         {
             Context = context;
             ProfileList = ProfileItem.GetProfileTree(context.Profiles);
-
+            PropertyChanged += OnPropertyChanged;
             context.ActiveProfileChangedEvent += OnActiveProfileChangedEvent;
+        }
+
+        private void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (nameof(SelectedProfileItem).Equals(e.PropertyName))
+            {
+                BuildDeviceLists();
+            }
+        }
+
+        private void BuildDeviceLists()
+        {
+            InputDevices = new ObservableCollection<DeviceItem>();
+            OutputDevices = new ObservableCollection<DeviceItem>();
+            if (SelectedProfileItem == null) return;
+
+            foreach (var inputDevice in GetDevices(SelectedProfileItem.Profile, DeviceIoType.Input))
+            {
+                InputDevices.Add(new DeviceItem(inputDevice));
+            }
+            foreach (var outputDevice in GetDevices(SelectedProfileItem.Profile, DeviceIoType.Output))
+            {
+                OutputDevices.Add(new DeviceItem(outputDevice));
+            }
+
+            OnPropertyChanged(nameof(InputDevices));
+            OnPropertyChanged(nameof(OutputDevices));
+        }
+
+        private List<Device> GetDevices(Profile profile, DeviceIoType deviceIoType)
+        {
+            return SelectedProfileItem.Profile.GetDeviceList(new DeviceBinding() {DeviceIoType = deviceIoType });
         }
 
         private void OnActiveProfileChangedEvent(Profile profile)
---
+++ UCR/ViewModels/Dashboard/DeviceItem.cs
@@ -0,0 +1,17 @@
+﻿using HidWizards.UCR.Core.Models;
+
+namespace HidWizards.UCR.ViewModels.Dashboard
+{
+    public class DeviceItem
+    {
+        public string Title => Device.Title;
+        public string ProviderName => Device.ProviderName;
+
+        private Device Device { get; set; }
+
+        public DeviceItem(Device device)
+        {
+            Device = device;
+        }
+    }
+}
