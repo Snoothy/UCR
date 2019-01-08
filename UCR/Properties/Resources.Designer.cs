58f82cc69b762d431030c5de18543a190766c478
Justin Tay <justintay123@gmail.com>
2019-01-08 21:53:11 +0800

    Add UCR icon to Resources

--- UCR/Properties/Resources.Designer.cs
+++ UCR/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace HidWizards.UCR.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon UCR {
+            get {
+                object obj = ResourceManager.GetObject("UCR", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
